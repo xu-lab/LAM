c54679ebdadc4622fda01a82b45f70c6e87145e6
Xu-lab @ CCHMC <xu-lab@users.noreply.github.com>
2020-03-20 10:47:45 -0400

    Create LAM1234.R

---
+++ LAM1234.R
@@ -0,0 +1,22 @@
+library(Seurat)
+library(ggplot2)
+
+db = "LAM1234"
+
+objlist = list(LAM1,LAM2,LAM3,LAM4)
+
+anchors <- FindIntegrationAnchors(object.list = objlist, 
+                                  dims = 1:30, 
+                                  anchor.features = 2000)
+obj <- IntegrateData(anchorset = anchors, dims = 1:30)
+
+obj = ScaleData(obj, vars.to.regress = "nCount_RNA")
+obj = RunPCA(obj, npcs=200)
+
+obj = RunUMAP(obj, reduction="pca", dims=1:30, umap.method="uwot", 
+              min.dist=0.2, n.neighbors = 30, metric="cosine", n.epochs =500)
+
+obj = FindNeighbors(obj, reduction = "pca", dims=1:100) 
+obj = FindClusters(obj, resolution = 1.2)
+
+saveRDS(obj, file=paste0(db, ".rds"))
